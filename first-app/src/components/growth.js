ad1d540b13afb74c8cdd4f0e2ce3307e3d556cbd
Grace_Chae <eunhye.chae@edu.sait.ca>
2026-01-11 20:24:59 -0700

    updated health section

--- first-app/src/components/growth.js
+++ first-app/src/components/growth.js
@@ -1,6 +1,8 @@
 import OverviewCard from "../pages/overviewCard";
+import { useState } from "react";
 
 function Growth() {
+  const [lesson, setLesson] = useState("");
   return (
     <div style={{ display: "grid", gap: "16px", padding: "20px" }}>
       <OverviewCard
@@ -8,6 +10,16 @@ function Growth() {
         title="Growth"
         description="How well I took care of myself today"
       />
+
+      <div>
+        <label>What I learn today?</label> <br />
+        <textarea
+          placeholder="Write here..."
+          value={lesson}
+          onChange={(e) => setLesson(e.target.value)}
+          rows={6}
+        />
+      </div>
     </div>
   );
 }
